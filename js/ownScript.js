08b5a1ac2466afa9816f483c800e3659d7841f3f
Skamster <hersche@vmail.me>
2014-04-27 23:54:10 +0200

    cleanup, reorder and extend debug for strange github-bug
    on github, the site always tells me, request fails, but no error.. local
    = perfect and also on github just after first time. may also .appcache?

--- js/ownScript.js
+++ js/ownScript.js
@@ -1,16 +1,15 @@
 function processHtml(jDataIn, name) {
     var displayHtml = "";
     $.each(jDataIn, function (key, val) {
-        var site = jQuery.parseJSON(JSON.stringify(val));
-        if (name == key) {
-            if (key == "index") {
+        if (name === key) {
+            if (key === "index") {
                 displayHtml += "<h1>" + val.title + "</h1><p>" + val.description + "</p><table><tr><th>Projectname</th><th>Source</th>";
                 displayHtml += "<th>Docs</th><th>Stability</th></tr>";
                 $.each(val.content, function (cKey, cVal) {
                     displayHtml += '<tr><td><a href="index.html?s=' + cKey + '" onclick="changeContent(\'' + cKey + '\'); return false;" >' + cKey + '</a></td><td><a href="' + cVal.sLink + '">Src</a></td>';
                     displayHtml += "<td><a href='" + cVal.dLink + "'>Doc</a></td><td>" + cVal.stable + "</td></tr>";
                 });
-            } else if (key == "about") {
+            } else if (key === "about") {
                 displayHtml += '<p><img src="' + val.picture + '"/><br />' + val.description + '</p><ul>';
                 $.each(val.contact, function (cKey, cVal) {
                     displayHtml += '<li><b>' + cKey + '</b>: ' + cVal + '</li>';
@@ -35,4 +34,30 @@ function processHtml(jDataIn, name) {
         }
     });
     return displayHtml;
+}
+
+function updateContent() {
+    $.getJSON(jsonFile)
+        .done(function (data) {
+            jData = jQuery.parseJSON(JSON.stringify(data));
+            if (typeof (Storage) !== "undefined") {
+                localStorage.removeItem("jData");
+                localStorage.jData = JSON.stringify(data);
+            }
+            $("#showContent").html(processHtml(jData, lastName));
+
+        })
+        .fail(function (jqxhr, textStatus, error) {
+            alert("Request failed: " + error+textStatus+jqxhr);
+            console.log("Request Failed: " + error+textStatus+jqxhr);
+        });
+
+
+}
+
+function GET(v) {
+    if(!HTTP_GET_VARS[v]) {
+        return 'undefined';
+    }
+    return HTTP_GET_VARS[v];
 }
