50d4f9c634568a2443da8f4e90e54ee20af5e6a5
Vinzenz Hersche <hersche@vmail.me>
2014-06-29 13:21:02 +0200

    implement timeline-js

--- js/ownScript.js
+++ js/ownScript.js
@@ -1,13 +1,25 @@
+function createTimeline(tlD, lang) {
+        createStoryJS({
+        type:       'timeline',
+        width:      '800',
+        height:     '500',
+        source:     tlD,
+        lang:       lang,
+        embed_id:   'my-timeline',
+        debug: 'true'
+    });
+}
+
 function processMenuHtml(jDataIn) {
     var menuHtml = '<ul style="display: inline; margin:0; list-style: none;" >';
-    $.each(jDataIn, function(menuName, value) {
+    $.each(jDataIn, function (menuName, value) {
         var parent = "";
         if (menuName != "transl") {
             if (menuName == "index") {
                 styleClass = '"submenu"';
                 parent = ' class="dropdowncontainer" ';
             }
-            menuHtml += '<li' + parent + ' ><a href="index.html?s=' + menuName + '" data-tool="' + value.preword + '" onclick="changeContent(\'' + menuName + '\'); return false;" class="btn fade">' + value.title + '</a>';
+            menuHtml += '<li' + parent + ' ><a href="#" data-tool="' + value.preword + '" onclick="changeContent(\'' + menuName + '\'); return false;" class="btn fade">' + value.title + '</a>';
             // This is because i know, index is the first.. it have to be!	  
             if (menuName != "index") {
                 menuHtml += '</li>';
@@ -27,25 +39,20 @@ function processHtml(jDataIn, name) {
     "use strict";
     var displayHtml = '<div style="margin-top:10%;">';
     var toc = "";
-    $.each(jDataIn, function(key, val) {
+    $.each(jDataIn, function (key, val) {
         if (name === key) {
             if (key === "index") {
                 displayHtml += "<h2>" + val.title + "</h2><p>" + val.preword + '</p><a name="TheTable" class="anchor" ><a/><table><tr><th>' + jDataIn.transl.projectname + '</th><th>' + jDataIn.transl.source + '</th>';
                 displayHtml += "<th>" + jDataIn.transl.doc + "</th><th>" + jDataIn.transl.stability + "</th></tr>";
-                $.each(val.content, function(cKey, cVal) {
+                $.each(val.content, function (cKey, cVal) {
                     displayHtml += '<tr><td><a href="index.html?s=' + cVal.pLink + '" onclick="changeContent(\'' + cVal.pLink + '\'); return false;" >' + cKey + '</a></td><td><a href="' + cVal.sLink + '">Src</a></td>';
                     displayHtml += "<td><a href='" + cVal.dLink + "'>Doc</a></td><td>" + cVal.stable + "</td></tr>";
                 });
                 displayHtml += '</table><p><a name="description" class="anchor" ></a>' + val.description + '</p>';
-                toc += '<ul><li><a href="#TheTable">' + jDataIn.transl.table + '</a></li><li><a href="#description">' + jDataIn.transl.description + '</a></li></ul>';
-            } 
-            
-            
-            
-            
-            else if (key === "about") {
-                displayHtml += '<div class="h-card" <p><img class="u-photo" id="aboutImg" src="' + val.picture + '"/><br />' + val.description + '</p><ul>';
-                $.each(val.contact, function(cKey, cVal) {
+                toc += '<ul><li><a href="#TheTable">' + jDataIn.transl.table + '</a></li><li><a href="#description">' + jDataIn.transl.description + '</a><ul><li><a href="#website">Website</a></li><li><a href="#security">Sicherheit</a></li><li><a href="#about">Über mich</a></li></ul></li></ul>';
+            } else if (key === "about") {
+                displayHtml += '<div class="h-card" <p><img class="u-photo" id="aboutImg" src="' + val.picture + '"/><br />' + val.description + '</p><a id="timeline" href="#my-timeline" onclick="$(\'#my-timeline\').show()">'+jDataIn.transl.timeline+'</a><ul>';
+                $.each(val.contact, function (cKey, cVal) {
                     if (cKey === "E-Mail") {
                         displayHtml += '<li><b>' + cKey + '</b>: <a class="u-email" href="mailto:' + cVal + '">' + cVal + '</a></li>';
                     } else {
@@ -54,11 +61,11 @@ function processHtml(jDataIn, name) {
                 });
                 var langs = "";
                 toc += '<ul style="list-style-type: none; list-style: none;" ><li><a href="#languages">' + val.languages.trans + '</a></li><ul style="list-style-type: none; list-style: none;">';
-                $.each(val.languages, function(lKey, lVal) {
+                $.each(val.languages, function (lKey, lVal) {
                     if (lKey != "trans") {
                         toc += '<li><a href="#' + lKey + '">' + lKey + '</a></li>';
                         langs += '<li><b>' + lKey + '</b><a name="' + lKey + '" class="anchor" />: ' + lVal.desc + '<ul style="margin-bottom:10px; list-style-type: none; list-style: none;">';
-                        $.each(lVal.frameworks, function(fKey, fVal) {
+                        $.each(lVal.frameworks, function (fKey, fVal) {
                             langs += '<li style="margin-bottom:5px;">' + fKey + ': ' + fVal + '</li>';
                         });
                         langs += '</ul></li>';
@@ -67,12 +74,12 @@ function processHtml(jDataIn, name) {
 
                 toc += '</ul><li><a href="#sys">' + val.sys.trans + '</a></li><ul>';
                 var syst = "<ul>";
-                $.each(val.sys, function(sKey, sVal) {
+                $.each(val.sys, function (sKey, sVal) {
                     if ((sKey != "trans") && (sKey != "centossub")) {
                         if (sKey === "CentOS") {
                             toc += '<li><a href="#' + sKey + '">' + sKey + '</a></li>';
                             syst += '<li><b>' + sKey + '</b>: ' + sVal + '<a name="' + sKey + '" class="anchor" /></li><ul>';
-                            $.each(val.sys.centossub, function(subKey, subValue) {
+                            $.each(val.sys.centossub, function (subKey, subValue) {
                                 syst += '<li><b>' + subKey + '</b><a name="' + subKey + '" class="anchor" />: ' + subValue + '</li>';
                             });
                             syst += '</ul>';
@@ -84,7 +91,7 @@ function processHtml(jDataIn, name) {
                 });
                 toc += '</ul><li><a href="#mobile">' + val.mobile.trans + '</a></li><ul>';
                 var mobile = "<ul>";
-                $.each(val.mobile, function(mKey, mVal) {
+                $.each(val.mobile, function (mKey, mVal) {
                     if (mKey != "trans") {
                         toc += '<li><a href="#' + mKey + '">' + mKey + '</a></li>';
                         mobile += '<li><b>' + mKey + '</b><a name="' + mKey + '" class="anchor" />: ' + mVal + '</li>';
@@ -93,24 +100,24 @@ function processHtml(jDataIn, name) {
 
                 toc += '</ul><li><a href="#support">' + val.support.trans + '</a></li><ul>';
                 var support = "<ul>";
-                $.each(val.support.reference, function(sKey, sVal) {
+                $.each(val.support.reference, function (sKey, sVal) {
                     toc += '<li><a href="#' + sKey + '">' + sKey + '</a></li>';
                     support += '<li><b>' + sKey + '</b><a name="' + sKey + '" class="anchor" />: ' + sVal + '</li>';
                 });
-                
-                
+
+
                 toc += '</ul><li><a href="#oldjobs">' + val.oldjobs.trans + '</a></li><ul>';
                 var oldjobs = '<p><a name="oldjobs" class="anchor" />' + val.oldjobs.description + '</p>';
-                
+
                 toc += '</ul><li><a href="#hobbys">' + val.hobbys.trans + '</a></li><ul>';
                 var hobbys = "<ul>";
-                $.each(val.hobbys, function(hKey, hVal) {
+                $.each(val.hobbys, function (hKey, hVal) {
                     if (hKey != "trans") {
                         toc += '<li><a href="#' + hKey + '">' + hKey + '</a></li>';
                         hobbys += '<li><b>' + hKey + '</b><a name="' + hKey + '" class="anchor" />: ' + hVal + '</li>';
                     }
                 });
-                
+
                 toc += "</ul></ul>";
                 //displayHtml += "</div>"+toc+"</ul>";
                 displayHtml += '</ul><h3><a name="languages" class="anchor" />' + val.languages.trans + '</h3><ul>';
@@ -136,18 +143,18 @@ function processHtml(jDataIn, name) {
                 toc += '<li><a href="#bugs">' + jDataIn.transl.bugs + '</a></li><li><a href="#description">' + jDataIn.transl.description + '</a></li></ul>';
                 displayHtml += '<a href="index.html" onclick="changeContent(\'index\'); return false">Links</a>';
                 displayHtml += '<h3><a name="features" class="anchor" />' + jDataIn.transl.features + '</h3><ul>';
-                $.each(val.features, function(fKey, fVal) {
+                $.each(val.features, function (fKey, fVal) {
                     displayHtml += "<li>" + fKey + ": " + fVal + "</li>";
                 });
 
                 if (pictureCount > 0) {
                     displayHtml += '</ul><h3><a name="pictures" class="anchor" />' + jDataIn.transl.pictures + '</h3><ul>';
-                    $.each(val.pictures, function(pKey, pVal) {
+                    $.each(val.pictures, function (pKey, pVal) {
                         displayHtml += '<li>' + pVal.desc + ': <br /><a href="' + pVal.link + '" title="' + pVal.desc + '" class="gallerybox" data-fancybox-group="gallery"><img class="projectpic" src="' + pVal.src + '" /></a></li>';
                     });
                 }
                 displayHtml += '</ul><h3 ><a name="bugs" class="anchor" />' + jDataIn.transl.bugs + '</h3><ul>';
-                $.each(val.bugs, function(bKey, bVal) {
+                $.each(val.bugs, function (bKey, bVal) {
                     displayHtml += "<li>" + bKey + ": " + bVal + "</li>";
                 });
                 displayHtml += '</ul><h3><a name="description" class="anchor" />' + jDataIn.transl.description + '</h3><p>' + val.description + '</p>';
@@ -166,11 +173,12 @@ function processHtml(jDataIn, name) {
 
 function updateContent() {
     "use strict";
-    var jqxhr = $.getJSON(jsonFile, function() {});
-    jqxhr.complete(function(data) {
-        if (typeof(Storage) !== "undefined") {
+    var jqxhr = $.getJSON(jsonFile, function () {});
+    jqxhr.complete(function (data) {
+        if (typeof (Storage) !== "undefined") {
             localStorage.removeItem("jData");
             localStorage.removeItem("storeDate");
+            localStorage.removeItem("tlData");
             location.reload();
         }
     });
@@ -193,21 +201,16 @@ function changeContent(name) {
         }, "skamster.github.io::" + name, "index.html?s=" + name);
     }
     if (name != lastName) {
-        
-        $("#animationContainer").effect("slide", {
-            "direction": "right",
-            "mode": "hide"
-        }, 500, function() {
-            var ProcessedHtml = processHtml(jData, name);
-            $("#showContent").html(ProcessedHtml[0]);
-            $("#toc").html(ProcessedHtml[1]);
-            $("html, body").animate({ scrollTop: 0 }, "slow");
-        });
-
-        $("#animationContainer").effect("slide", {
-            "direction": "right",
-            "mode": "show"
-        }, 500);
+        $( "#animationContainer" ).fadeOut( "slow", function() {
+        // Animation complete.
+        var ProcessedHtml = processHtml(jData, name);
+        $("#showContent").html(ProcessedHtml[0]);
+        $("#toc").html(ProcessedHtml[1]);
+});
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
+        $("#animationContainer").fadeIn(1000);
         lastName = name;
     }
 
